747c2fa029b68e931dbcb8b9bb65fd6f0beccf4a
Andy McCall <mailme@andymccall.co.uk>
2025-06-15 10:48:05 +0100

    chore(): more work

--- src/main.asm
+++ src/main.asm
@@ -8,28 +8,80 @@
 ; API includes
    .include "includes/api/br_text.inc"
    .include "includes/api/br_screen.inc"
+   .include "includes/api/br_graphics.inc"
 
 ; App includes
    .include "includes/app/menu.inc"
-;   .include "includes/app/full_screen.inc"
-;   .include "includes/app/small_screen.inc"
+   .include "includes/app/full_screen.inc"
+   .include "includes/app/small_screen.inc"
 
+;---------------------------------------------------------------
+; Menu equates
+;---------------------------------------------------------------
+;
 start:
 
-  lda API_SOUND_CH_00    ; sound channel               (API::sound->play->channel)
-  sta API_PARAMETERS + 0 ; set API 'Parameter0'        (API::sound->play->channel)
-  lda API_SFX_COIN       ; sound effect index          (API::sound->play->effect)
-  sta API_PARAMETERS + 1 ; set API 'Parameter1'        (API::sound->play->effect)
-  lda API_FN_PLAY_SOUND  ; sound effect function       (API::sound->play)
-  sta API_FUNCTION       ; set API 'Function'          (API::sound->play)
-  lda API_GROUP_SOUND    ; 'Sound' API function group  (API::sound)
-  sta API_COMMAND        ; trigger 'Sound' API routine (API::sound)
+    jsr br_screen_clear
 
-    br_screen_clear
+app_loop:
+
+    jsr br_wait_for_vsync
 
     jsr menu_draw
 
-end:
-    jmp end                ; infinite loop
+    lda #API_FN_READ_CHAR
+    sta API_FUNCTION
+@wait_api:
+    lda API_COMMAND
+    bne @wait_api
+    lda #API_GROUP_CONSOLE
+    sta API_COMMAND
 
-loading_msg:     .byte "Loading Bitriot.dev...",0
+    lda API_PARAMETERS + 0
+
+menu_full_screen:
+   cmp #MENU_FULL_SCREEN
+   bne menu_small_screen
+   jsr show_full_screen
+   bra done
+menu_small_screen:
+   cmp #MENU_SMALL_SCREEN
+   bne menu_clear_screen
+   jsr show_small_screen
+   bra done
+menu_clear_screen:
+   cmp #MENU_CLEAR_SCREEN
+   bne menu_quit
+   jsr clear_screen
+   bra done
+menu_quit:
+   cmp #MENU_QUIT
+   beq quit
+done:
+   jmp app_loop
+
+quit:
+    jsr br_screen_clear
+@wait_api:
+    lda API_COMMAND
+    bne @wait_api
+    lda #API_FN_RESET
+    sta API_FUNCTION
+    lda #API_GROUP_SYSTEM
+    sta API_COMMAND
+    rts
+
+show_full_screen:
+    jsr br_screen_clear
+    full_screen
+    rts
+
+show_small_screen:
+    jsr br_screen_clear
+    small_screen
+    rts
+
+clear_screen:
+    jsr br_screen_clear
+    rts
+
